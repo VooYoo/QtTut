d5bf371c7987667596505209736b96b373e48b28
VooYoo <jakubosuch@gmail.com>
2018-12-12 21:21:28 +0100

    .

---
+++ QtDesigner/test.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'C:\GitHub\QtTut\QtDesigner\test.ui'
+#
+# Created by: PyQt4 UI code generator 4.11.4
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt4 import QtCore, QtGui
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    def _fromUtf8(s):
+        return s
+
+try:
+    _encoding = QtGui.QApplication.UnicodeUTF8
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+except AttributeError:
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig)
+
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName(_fromUtf8("Form"))
+        Form.resize(857, 530)
+        self.verticalLayoutWidget = QtGui.QWidget(Form)
+        self.verticalLayoutWidget.setGeometry(QtCore.QRect(450, 140, 175, 80))
+        self.verticalLayoutWidget.setObjectName(_fromUtf8("verticalLayoutWidget"))
+        self.vert_1 = QtGui.QVBoxLayout(self.verticalLayoutWidget)
+        self.vert_1.setObjectName(_fromUtf8("vert_1"))
+        self.hor_1 = QtGui.QHBoxLayout()
+        self.hor_1.setObjectName(_fromUtf8("hor_1"))
+        self.radioButton = QtGui.QRadioButton(self.verticalLayoutWidget)
+        self.radioButton.setObjectName(_fromUtf8("radioButton"))
+        self.hor_1.addWidget(self.radioButton)
+        self.vert2 = QtGui.QVBoxLayout()
+        self.vert2.setObjectName(_fromUtf8("vert2"))
+        self.checkBox = QtGui.QCheckBox(self.verticalLayoutWidget)
+        self.checkBox.setObjectName(_fromUtf8("checkBox"))
+        self.vert2.addWidget(self.checkBox)
+        self.hor_1.addLayout(self.vert2)
+        self.vert_1.addLayout(self.hor_1)
+        self.pushButton = QtGui.QPushButton(self.verticalLayoutWidget)
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Maximum)
+        sizePolicy.setHorizontalStretch(20)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.pushButton.sizePolicy().hasHeightForWidth())
+        self.pushButton.setSizePolicy(sizePolicy)
+        self.pushButton.setMaximumSize(QtCore.QSize(50, 16777215))
+        self.pushButton.setAutoDefault(False)
+        self.pushButton.setDefault(False)
+        self.pushButton.setFlat(False)
+        self.pushButton.setObjectName(_fromUtf8("pushButton"))
+        self.vert_1.addWidget(self.pushButton)
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        Form.setWindowTitle(_translate("Form", "Form", None))
+        self.radioButton.setText(_translate("Form", "RadioButton", None))
+        self.checkBox.setText(_translate("Form", "CheckBox", None))
+        self.pushButton.setText(_translate("Form", "PushButton", None))
+
--- src/1.py
+++ src/1.py
@@ -1,11 +1,144 @@
 import sys
-from PyQt4 import QtGui
+from PyQt4 import QtGui, QtCore
 
-app = QtGui.QApplication(sys.argv)
 
-window = QtGui.QWidget()
-window.setGeometry(50, 50, 500, 300)
-window.setWindowTitle("VooYoo")
-window.show()
+class Window(QtGui.QMainWindow):
+    icon_path = r'../img/Geralt.jpg'
 
-sys.exit(app.exec_())
+    def __init__(self):
+        super(Window, self).__init__()
+
+        self.set_window()
+
+        self.buttons()
+        self.progress_bar()
+        self.set_main_menu()
+        self.set_toolbar()
+
+        self.styles()
+        self.layout()
+        self.show()
+
+    def layout(self):
+        self.setGeometry(50, 50, 300, 220)
+
+        self.comboBox.move(20, 125)
+        self.styleChoice.move(20, 100)
+        self.progress_dwnld.setGeometry(20, 160, 250, 20)
+        self.btn_quit.setGeometry(150, 75, 75, 75)
+        self.check_box.move(20, 75)
+        self.btn_downld.move(20, 180)
+
+    def styles(self):
+        # QtGui.QApplication.setStyle(QtGui.QStyleFactory.create("Plastique"))
+        # todo
+        print(self.style().objectName())
+        self.styleChoice = QtGui.QLabel("costam", self)
+        print(QtGui.QStyleFactory.keys())
+        self.comboBox = QtGui.QComboBox(self)
+        self.comboBox.addItem("motif")
+        self.comboBox.addItem("Windows")
+        self.comboBox.addItem("cde")
+        self.comboBox.addItem("Plastique")
+        self.comboBox.addItem("Cleanlooks")
+        self.comboBox.addItem("windowsvista")
+        self.comboBox.addItem('WindowsXP')
+        self.comboBox.activated[str].connect(self.style_choice)
+
+    def style_choice(self, text):
+        self.styleChoice.setText(text)
+        QtGui.QApplication.setStyle(QtGui.QStyleFactory.create(text))
+
+    def progress_bar(self):
+        self.progress_dwnld = QtGui.QProgressBar(self)
+
+    def buttons(self):
+        self.btn_quit = QtGui.QPushButton('Quit', self)
+        self.btn_quit.clicked.connect(self.close_app)
+        # print(self.btn_quit.sizeHint())
+        # print(self.btn_quit.minimumSizeHint())
+        # print(self.btn_quit.size())
+
+        self.check_box = QtGui.QCheckBox('Enlarge Window', self)
+        self.check_box.stateChanged.connect(self.enlarge_window)
+
+        self.btn_downld = QtGui.QPushButton('Download', self)
+        self.btn_downld.clicked.connect(self.download)
+
+    def enlarge_window(self, state):
+        if state == QtCore.Qt.Checked:
+            self.setGeometry(50, 50, 1000, 600)
+        else:
+            self.setGeometry(50, 50, 500, 300)
+
+    def set_window(self):
+        self.setWindowTitle('VooYoo')
+        self.setWindowIcon(QtGui.QIcon(self.icon_path))
+
+    def set_main_menu(self):
+        menu_exit = QtGui.QAction('Exit', self)
+        menu_exit.setShortcut("q")
+        menu_exit.setStatusTip('Exit')
+        menu_exit.triggered.connect(self.close_app)
+        menu_exit.setIcon(QtGui.QIcon(self.icon_path))
+
+        self.statusBar()
+
+        main_menu = self.menuBar()
+        file_menu = main_menu.addMenu('File')
+        file_menu.addAction(menu_exit)
+
+    def set_toolbar(self):
+        tlbr_exit = QtGui.QAction(QtGui.QIcon(self.icon_path), 'Exit', self)
+        tlbr_exit.triggered.connect(self.close_app)
+
+        self.toolbar = self.addToolBar("Extraction")
+        self.toolbar.addAction(tlbr_exit)
+
+        fontChoice = QtGui.QAction('Font', self)
+        fontChoice.triggered.connect(self.font_choice)
+
+        # self.toolbar = self.addToolBar("Font")
+        self.toolbar.addAction(fontChoice)
+
+        # color = QtGui.QColor(0, 0, 0)
+        #
+        # fontColor = QtGui.QAction('Font bg Color', self)
+        # fontColor.triggered.connect(self.color_picker)
+        #
+        # self.toolbar.addAction(fontColor)
+        #
+        # cal = QtGui.QCalendarWidget(self)
+        # cal.setGeometry(500, 200, 200, 200)
+
+    def color_picker(self):
+        color = QtGui.QColorDialog.getColor()
+        self.styleChoice.setStyleSheet("QWidget { background-color: %s}" % color.name())
+
+    def font_choice(self):
+        font, valid = QtGui.QFontDialog.getFont()
+        if valid:
+            self.styleChoice.setFont(font)
+            QtGui.QApplication.setFont(font)
+
+    def download(self):
+        self.completed = 0
+
+        while self.completed < 100:
+            self.completed += 0.00001
+            self.progress_dwnld.setValue(self.completed)
+
+    def close_app(self):
+        choice = QtGui.QMessageBox.question(self, 'Exit', 'Exit?', QtGui.QMessageBox.Yes | QtGui.QMessageBox.No)
+        if choice == QtGui.QMessageBox.Yes:
+            sys.exit()
+
+
+def main():
+    app = QtGui.QApplication(sys.argv)
+    _ = Window()
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    main()
